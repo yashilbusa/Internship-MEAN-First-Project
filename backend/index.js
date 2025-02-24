1571b64004cb69196654200a8816890ff4e7961e
yashilbusa <yashilpatel1914@gmail.com>
2025-02-24 15:48:51 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,26 @@
 import express from 'express'
 import cors from 'cors'
-const app = express()
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 const port = 3012
 
+const app = express()
 app.use(cors());
+dotenv.config()
+
+try{
+    await mongoose.connect(process.env.url)
+    console.log('Connected to MongoDB');
+
+    app.listen(port,(req,res)=>{
+        console.log(`Server Running on http://localhost:${port}`);
+    });
+}catch(err){
+    console.log(`Error Connecting to MongoDB ${err.message}`)
+}
+
 
 app.get("/",(req,res)=>{
     res.send("Hello Yashil, It is Backend Server")
-})
-
-app.listen(port,(req,res)=>{
-    console.log(`Server Running on http://localhost:${port}`);
 })
 
